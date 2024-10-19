78c523bf1ba853722d6ff66fac6104f3f017d770
a.salvasser <a.salvasser@nixys.ru>
2024-10-20 00:56:19 +0700

    test template

--- Message-template/main.tf
+++ Message-template/main.tf
@@ -2,15 +2,15 @@ resource "grafana_message_template" "message" {
   name     = var.name
   disable_provenance = var.editable
   template = <<EOT
-  {{ define "firing message template" }}
-  🔥{{- .Labels.alertname }}
-  Summary: {{ .Annotations.summary }}
-  Instance {{ .Labels.instance }}: {{ printf "%.4f" .Values.B }}
-  {{ end }}
+{{ define "firing message template" }}
+🔥{{- .Labels.alertname }}
+Summary: {{ .Annotations.summary }}
+Instance {{ .Labels.instance }}: {{ printf "%.4f" .Values.B }}
+{{ end }}
 
-  {{ define "resolved message template" }}
-  ✅{{- .Labels.alertname }}
-  Instance {{ .Labels.instance }}
-  {{ end }}
-  EOT
+{{ define "resolved message template" }}
+✅{{- .Labels.alertname }}
+Instance {{ .Labels.instance }}
+{{ end }}
+EOT
 }
